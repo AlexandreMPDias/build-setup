995060c2063930988138d6eb57f8146648704cb0
AlexandreMPDias <alexandrempdias@icloud.com>
2020-12-29 17:25:39 -0300

    set node main version to latest

--- win_x64/bin/step_2.bat
+++ win_x64/bin/step_2.bat
@@ -4,7 +4,7 @@ cd %~dp0
 setx /M PATH "%PATH%;C:\Dev\win-scripts\Scripts\bin;C:\Dev\Gow\bin"
 
 
-nvm install --lts
+nvm install lastest
 npm install -g https://github.com/AlexandreMPDias/wml alex-test-cli typescript yarn
 
 nvm install 10.18.1
